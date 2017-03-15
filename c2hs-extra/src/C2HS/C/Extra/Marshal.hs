d3b3363721228665a5ac34068bef751d9080bf1d
Tao He <sighingnow@gmail.com>
2017-03-15 18:41:27 +0800

    Fix a dangling pointer bug in c2hs-extra.

--- c2hs-extra/src/C2HS/C/Extra/Marshal.hs
+++ c2hs-extra/src/C2HS/C/Extra/Marshal.hs
@@ -61,9 +61,20 @@ peekStringArray n p = if p == nullPtr
 -- | Use an array of String as argument, usually used to pass multiple names to C
 -- functions.
 withStringArray :: [String] -> (Ptr (Ptr CChar) -> IO a) -> IO a
+withStringArray [] f = f nullPtr
 withStringArray ss f = do
     ps <- mapM (\s -> withCString s return) ss
     withArray ps f
+
+{-
+-- | Peek an array of Integral and the result's length is given.
+peekStringArray :: Integral n => n -> Ptr (CInt) -> IO [String]
+peekStringArray n p = peekArray (fromIntegral n) p >>= mapM peekCString
+
+{-# SPECIALIZE peekStringArray :: Int -> Ptr (Ptr CChar) -> IO [String] #-}
+{-# SPECIALIZE peekStringArray :: CUInt -> Ptr (Ptr CChar) -> IO [String] #-}
+
+-}
 
 -- | Use an array of Integral as argument.
 withIntegralArray :: (Integral a, Integral b, Storable b) => [a] -> (Ptr b -> IO c) -> IO c
