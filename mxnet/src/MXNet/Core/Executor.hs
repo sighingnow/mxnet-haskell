becbd6736e1b8e252f1bcf89d824d34243f5e193
Tao He <sighingnow@gmail.com>
2017-03-16 14:18:05 +0800

    Symbol module and executor module.

---
+++ mxnet/src/MXNet/Core/Executor.hs
@@ -0,0 +1,46 @@
+-----------------------------------------------------------
+-- |
+-- module:                      MXNet.Core.Symbol
+-- copyright:                   (c) 2016 Tao He
+-- license:                     MIT
+-- maintainer:                  sighingnow@gmail.com
+--
+-- Symbol module.
+--
+module MXNet.Core.Executor where
+
+import           Control.Exception
+import           Control.Monad
+import           MXNet.Core.Base
+import           MXNet.Core.NDArray (NDArray(NDArray), DType)
+import           MXNet.Core.NNVM.Base
+import           MXNet.Core.NNVM.Base.Internal.Raw
+
+-- | Type alias for variable.
+newtype Executor a = Executor { getHandle :: ExecutorHandle }
+
+-- | Make an executor using the given handler.
+makeExecutor :: DType a
+             => ExecutorHandle
+             -> IO (Executor a)
+makeExecutor = return . Executor
+
+-- | Executor forward method.
+forward :: DType a
+        => Executor a   -- ^ The executor handle.
+        -> Bool         -- ^ Whether this forward is for evaluation purpose.
+        -> IO ()
+forward exec train = void $ mxExecutorForward (getHandle exec) (if train then 1 else 0)
+
+-- | Executor backward method.
+backward :: DType a
+         => Executor a  -- ^ The executor handle.
+         -> IO ()
+backward exec = void $ mxExecutorBackward (getHandle exec) 0 []
+
+getOutput :: DType a
+          => Executor a
+          -> IO [NDArray a]
+getOutput exec = do
+    (_, outs) <- mxExecutorOutputs (getHandle exec)
+    return $ NDArray <$> outs
--- mxnet/src/MXNet/Core/Symbol.hs
+++ mxnet/src/MXNet/Core/Symbol.hs
@@ -7,28 +7,174 @@
 --
 -- Symbol module.
 --
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
+{-# OPTIONS_GHC -Wno-redundant-constraints #-}
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module MXNet.Core.Symbol where
 
-{-
-      -- (
-      -- * Functions about NDArray
-      -- * Default contexts
-      -- ) where
--}
-
+import           Control.Exception
 import           Control.Monad
+import           Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as HM
+import           Data.Monoid
+import           Foreign.Ptr (nullPtr)
+import           System.IO.Unsafe
+import           Unsafe.Coerce (unsafeCoerce)
 
 import           MXNet.Core.Base
+import           MXNet.Core.Executor hiding (getHandle)
+import qualified MXNet.Core.Executor as Executor (getHandle)
+import           MXNet.Core.NDArray hiding (getHandle)
+import qualified MXNet.Core.NDArray as NDArray (getHandle)
+import           MXNet.Core.NNVM.Base
+import           MXNet.Core.NNVM.Base.Internal.Raw
 
 -- | Type alias for variable.
-type Var = SymbolHandle
+newtype Symbol a = Symbol { getHandle :: SymbolHandle }
+
+instance DType a => Show (Symbol a) where
+    show sym = unsafePerformIO $ do
+        (_, str) <- mxSymbolPrint (getHandle sym)
+        return str
 
 -- | Make a new symbolic variable with given name.
-makeVar :: String   -- ^ Name.
-        -> IO Var   -- ^ Result variable.
-makeVar = mxSymbolCreateVariable >=> return . snd
+makeVar :: DType a
+        => String           -- ^ Name.
+        -> IO (Symbol a)    -- ^ Result variable.
+makeVar = mxSymbolCreateVariable >=> return . Symbol . snd
 
 -- | Get the name of a given variable.
-getVarName :: Var -> IO String
-getVarName = mxSymbolGetName >=> \(_, name, _) -> return name
+name :: DType a => Symbol a -> IO String
+name = mxSymbolGetName . getHandle >=> \(_, nm, _) -> return nm
 
+-- | Get some attribute of symbol.
+attr :: DType a => Symbol a -> String -> IO (Maybe String)
+attr sym key = do
+    (_, s, success) <- mxSymbolGetAttr (getHandle sym) key
+    return $ if success == 0    -- 0 when success, -1 when failure happens
+                then Just s
+                else Nothing
+
+-- | Infer the type of the given symbol, return arg_types, out_types and aux_types.
+inferType :: DType a => Symbol a -> [String] -> IO ([Int], [Int], [Int])
+inferType sym args = do
+    (_, arg, out, aux) <- mxSymbolInferType (getHandle sym) args
+    return (arg, out, aux)
+
+-- | Infer the shape of the given symbol, return the in, out and auxiliary shape size.
+inferShape :: DType a => Symbol a -> [String] -> IO ([[Int]], [[Int]], [[Int]])
+inferShape sym args = do
+    (_, arg, out, aux) <- mxSymbolInferShape (getHandle sym) args [0] []
+    return (arg, out, aux)
+
+-- | List all input arguments.
+getInputs :: DType a => Symbol a -> IO [String]
+getInputs sym = snd <$> mxSymbolListArguments (getHandle sym)
+
+-- | List all output results.
+getOutputs :: DType a => Symbol a -> IO [String]
+getOutputs sym = snd <$> mxSymbolListOutputs (getHandle sym)
+
+-- | List all auxiliary states.
+getAuxiliary :: DType a => Symbol a -> IO [String]
+getAuxiliary sym = snd <$> mxSymbolListAuxiliaryStates (getHandle sym)
+
+-- | Compose multiple symbols.
+--
+-- This function will change the sym hanlde. To achieve function apply behavior,
+-- copy the symbol first before apply.
+compose :: DType a => Symbol a -> String -> [Symbol a] -> HashMap String (Symbol a) -> IO ()
+compose var name args kwargs = do
+    when ((not . null) args && (not . HM.null) kwargs) $
+        throwIO $ userError "compose only accept input Symbols either as positional or keyword arguments, not both"
+    let (arg_names, arg_values) = if null args
+                                     then (HM.keys kwargs, HM.elems kwargs)
+                                     else ([], args)
+        nargs = if null args
+                   then fromIntegral (length kwargs)
+                   else fromIntegral (length args)
+    void $ nnSymbolCompose (getHandle var) name nargs arg_names (getHandle <$> arg_values)
+
+createAtomic :: DType a => String -> OpHandle -> [Symbol a] -> [String] -> [String] -> IO (Symbol a)
+createAtomic name op args keys values = do
+    let nargs = fromIntegral (length keys)
+    (_, h) <- mxSymbolCreateAtomicSymbol op nargs keys values
+    let sym = Symbol h
+    compose sym name args [] >> return sym
+
+addition :: IO OpHandle
+addition = do
+    (_, handle) <- nnGetOpHandle "elemwise_add"
+    return handle
+
+plusvar :: DType a => Symbol a -> Symbol a -> IO (Symbol a)
+plusvar a b = do
+    handle <- addition
+    var <- createAtomic "sum" handle [a, b] [] []
+    nnSymbolListOutputNames (getHandle var) >>= print
+    nnSymbolListInputNames (getHandle var) 0 >>= print
+    return var
+
+-- | Get the autodiff of current symbol.
+-- This function can only be used if current symbol is a loss function.
+grad :: DType a => Symbol a -> [String] -> IO (Symbol a)
+grad sym args = do
+    let nargs = fromIntegral (length args)
+    (_, handle) <- mxSymbolGrad (getHandle sym) nargs args
+    return $ Symbol handle
+
+-- | Bind with explicit argument mapping (name -- value mapping).
+bind :: DType a
+     => Symbol a
+     -> Context
+     -> HashMap String (NDArray a)
+     -> IO (Executor a)
+bind sym Context{..} args = do
+    inputs <- genNDArrayMapping args <$> getInputs sym
+    -- req_map = {'null': 0, 'write': 1, 'add': 3}
+    let req_types = replicate (HM.size inputs) 1        -- use default value.
+    (_, exec) <- mxExecutorBind (getHandle sym)
+                                deviceType
+                                deviceId
+                                (fromIntegral (HM.size inputs))     -- length of input arguments.
+                                (NDArray.getHandle <$> HM.elems inputs)
+                                (replicate (HM.size inputs) (unsafeCoerce nullPtr))
+                                req_types
+                                0                                   -- length of auxiliary states.
+                                []                                  -- no auxiliary states.
+    return $ Executor exec
+  where
+    -- | Get ndarray lists handles from input arguments.
+    genNDArrayMapping args arg_names = HM.fromList (genfn <$> arg_names)
+      where
+        genfn nm = case HM.lookup nm args of
+                        Just v -> (nm, v)
+                        Nothing -> throw . userError $ "getNDArrayInputs: no argument " <> nm
+
+-- | Bind without explicit argument mapping (name -- value mapping).
+bind' :: DType a
+      => Symbol a
+      -> Context
+      -> [NDArray a]
+      -> IO (Executor a)
+bind' sym Context{..} args = do
+    inputs <- genNDArrayMapping args <$> getInputs sym
+    -- req_map = {'null': 0, 'write': 1, 'add': 3}
+    let req_types = replicate (HM.size inputs) 1        -- use default value.
+    (_, exec) <- mxExecutorBind (getHandle sym)
+                                deviceType
+                                deviceId
+                                (fromIntegral (HM.size inputs))     -- length of input arguments.
+                                (NDArray.getHandle <$> HM.elems inputs)
+                                (replicate (HM.size inputs) (unsafeCoerce nullPtr))
+                                req_types
+                                0                                   -- length of auxiliary states.
+                                []                                  -- no auxiliary states.
+    return $ Executor exec
+  where
+    -- | Get ndarray lists handles from input arguments without explicit argument names.
+    genNDArrayMapping args names =
+        assert (length args == length names) $
+            HM.fromList (zip names args)
